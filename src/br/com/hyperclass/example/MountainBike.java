69190ed5a01c7e6f052babc714b8fd291a8a1fcf
Arllen Lira <arllen.lira@focusnetworks.com.br>
2017-05-16 17:15:07 -0300

    Java example for Polimorphism

---
+++ src/br/com/hyperclass/example/MountainBike.java
@@ -0,0 +1,25 @@
+package br.com.hyperclass.example;
+
+public class MountainBike extends Bicycle {
+
+	private String suspension;
+
+	public MountainBike(int startCadence, int startSpeed, int startGear, String suspensionType) {
+		super(startCadence, startSpeed, startGear);
+		this.setSuspension(suspensionType);
+	}
+
+	public String getSuspension() {
+		return this.suspension;
+	}
+
+	public void setSuspension(String suspensionType) {
+		this.suspension = suspensionType;
+	}
+
+	public void printDescription() {
+		super.printDescription();
+		System.out.println("The " + "MountainBike has a " + getSuspension() + " suspension.");
+	}
+
+}
